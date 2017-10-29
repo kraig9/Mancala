bb8723a8c4d8f35b0a6ed82db9beefd9a4f90e7f
kwo227 <kwo227@tamu.edu>
2017-10-29 16:33:49 -0500

    Started wotking on game board

--- Kalah/src/Mancala/Game.java
+++ Kalah/src/Mancala/Game.java
@@ -26,3 +26,53 @@ public class Game
 		});
 	}
 }
+
+class Peices {
+	
+}
+
+class Board {
+	public enum Hole{
+		//player one's holes and pit
+		one,
+		two,
+		three,
+		four,
+		five,
+		six,
+		//player two's holes and pit
+		seven,
+		eight,
+		nine,
+		ten,
+		eleven,
+		twelve;
+		
+		//start with 4 marbles in each hole
+		public static final int START_MARBLES = 4;
+		//holds the amount of marbles currently in hole
+		private double numMarbles;
+		Hole (){
+			this.numMarbles = START_MARBLES;
+		}
+	}
+	class pit1{
+		int numMarbles;
+		
+		void addMarbles(){
+			numMarbles++;
+		}
+	}
+	class pit2{
+		int numMarbles;
+		
+		void addMarbles() {
+			numMarbles++;
+		}
+	}
+	
+}
+
+class Scoreboard {
+	
+}
